c5cb2a82ae82d005c193996903cc840639d56ca2
Liam Volschenk <liamvolschenk@gmail.com>
2021-08-16 18:09:03 +0200

    server update

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
+//importing the necesary modules for our server file
 import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
+//importing error handlers
 import {
   notFound,
   errorHandler
 } from './middleware/errorMiddleware.js'
+//importing db connection and routes
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+
 
 dotenv.config()
 
@@ -22,19 +26,12 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json())
-
+//making use of everything that was imported
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
-
-/*const __dirname = path.resolve()
+//serve frontend/build as static assets
+const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
@@ -47,11 +44,13 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/', (req, res) => {
     res.send('API is running....')
   })
-}*/
+}
 
+//using the error handling files
+app.use(notFound)
+app.use(errorHandler)
 
-
-
+//setting the port
 const PORT = process.env.PORT || 5000
 
 app.listen(
