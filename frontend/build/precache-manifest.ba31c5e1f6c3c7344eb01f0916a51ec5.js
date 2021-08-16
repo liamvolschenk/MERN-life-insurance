e29756e4cc1769be18d3a7de0267e206d35006fa
Liam Volschenk <liamvolschenk@gmail.com>
2021-08-16 16:13:20 +0200

    server update

---
+++ frontend/build/precache-manifest.ba31c5e1f6c3c7344eb01f0916a51ec5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f13e98c764a54ca4214eeeb67ef6b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae2a0b2627a71ff1437f",
+    "url": "/static/css/main.3620056f.chunk.css"
+  },
+  {
+    "revision": "73a52aa860d632b12707",
+    "url": "/static/js/2.3d73e33b.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.3d73e33b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae2a0b2627a71ff1437f",
+    "url": "/static/js/main.e41c3067.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "e3c124e45583c841bd3ee30e4c1e35c5",
+    "url": "/static/media/about.e3c124e4.png"
+  },
+  {
+    "revision": "f80cf8401e558c1444b5d98557f72528",
+    "url": "/static/media/agent.f80cf840.jpeg"
+  },
+  {
+    "revision": "eb5166536a092dfb7c9ad68445b34fce",
+    "url": "/static/media/browse.eb516653.png"
+  },
+  {
+    "revision": "d39cb86cbbec4b20594e886b70f448bf",
+    "url": "/static/media/contact.d39cb86c.png"
+  },
+  {
+    "revision": "c13d41a70e5ed3d604b903ef30683cff",
+    "url": "/static/media/family.c13d41a7.png"
+  },
+  {
+    "revision": "73d3115515e4136aab089aa61d237859",
+    "url": "/static/media/logo.73d31155.png"
+  },
+  {
+    "revision": "6fbb439f8371aa89ed09651e6a8480a8",
+    "url": "/static/media/park.6fbb439f.jpeg"
+  },
+  {
+    "revision": "249db5a3ab9cedfe5d762dd053246e3e",
+    "url": "/static/media/plans.249db5a3.png"
+  },
+  {
+    "revision": "eca777b3e088c8bc4cb9b0b608bc9ef7",
+    "url": "/static/media/select.eca777b3.png"
+  }
+]);
