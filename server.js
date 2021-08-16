36a9fabab5576078eb7e4e4d9c3e070792f5137b
Liam Volschenk <liamvolschenk@gmail.com>
2021-08-16 14:26:09 +0200

    initial commit

--- server.js
+++ server.js
@@ -45,10 +45,6 @@ if (process.env.NODE_ENV === 'production') {
   })
 }*/
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 //setting the port
 const PORT = process.env.PORT || 5000
