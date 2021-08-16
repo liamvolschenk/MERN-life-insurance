fe7803dbdbb9ad7d6e975649b122d282c9ce748a
Liam Volschenk <liamvolschenk@gmail.com>
2021-08-16 14:26:37 +0200

    server update

--- server.js
+++ server.js
@@ -45,6 +45,11 @@ if (process.env.NODE_ENV === 'production') {
   })
 }*/
 
+app.use(express.static(path.join(__dirname, "frontend", "build")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 //setting the port
 const PORT = process.env.PORT || 5000
